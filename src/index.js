2ac9964abded19a65b34a97eb04b839e13d36be9
hyunjun06 <junkayden@gmail.com>
2022-12-09 12:14:39 +0900

    created todo list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
-const plus = document.getElementById('plus');
-const minus = document.getElementById('minus');
-const counter = document.getElementById('counter');
+import { legacy_createStore as createStore } from 'redux';
 
-let count = 0;
+const todoList = document.getElementById('todo-list');
+const todoInput = document.getElementById('todo-input');
 
-const updateCounter = () => {
-  counter.innerText = count;
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, action.payload];
+    case DELETE_TODO:
+      return state.filter((_todo, index) => index !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const addTodo = (todo) => {
+  store.dispatch({
+    type: ADD_TODO,
+    payload: todo,
+  });
 }
 
-plus.addEventListener('click', () => {
-  count++;
-  updateCounter();
-});
+const deleteTodo = (index) => {
+  store.dispatch({
+    type: DELETE_TODO,
+    payload: index,
+  });
+}
 
-minus.addEventListener('click', () => {
-  count--;
-  updateCounter();
+const render = () => {
+  const state = store.getState();
+  todoList.innerHTML = '';
+  state.forEach((todo, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = todo;
+    li.addEventListener('click', () => deleteTodo(index));
+    todoList.appendChild(li);
+  });
+}
+
+store.subscribe(render);
+
+todoInput.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const todo = event.target.elements[0].value;
+  addTodo(todo);
+  event.target.reset();
 });
